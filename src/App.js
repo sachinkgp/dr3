caa24bdec9c30fd5b14f238c13e4bd4a72d74382
sachin <sachinsinghiitkgp@gmail.com>
2023-04-11 01:51:08 +0530

    cleaning code

--- src/App.js
+++ src/App.js
@@ -3,38 +3,7 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Signup from './components/signup';
 import Navbar from './components/navbar';
 import SignIn from './components/signin';
-import {useState,useEffect} from 'react';
-import axios from 'axios';
-// import AllOrders from './components/orders';
-
-const AllOrders=()=>{
-  const [orders,setOrders] = useState([])
-
-  useEffect(()=>{
-      axios
-      .get('http://jsonplaceholder.typicode.com/users')
-      .then(res=>{
-          setOrders(res.data);
-      })
-  })
-  
-  return(
-      <div>
-          {
-              orders.map(order=>(
-                  <div className='flex-container'>
-                      <div className='card'>
-                          <img className='imgZ' src="https://picsum.photos/300"/>
-                          <h1 className='row' key={order.id}>{order.name}</h1>
-                          <button className='buttonZ'> Add this order</button>
-                      </div>
-                  </div>
-              ))
-          }
-      </div>
-  
-  )
-}
+import AllOrders from './components/orders';
 
 function App() {
   return (
--- src/components/orders.js
+++ src/components/orders.js
@@ -1,34 +1,34 @@
-// import React,{useState,useEffect} from 'react'
-// import '../App.css'
-// import axios from 'axios'
+import React,{useState,useEffect} from 'react'
+import '../App.css'
+import axios from 'axios'
 
-// const AllOrders=()=>{
-//     const [orders,setOrders] = useState([])
+const AllOrders=()=>{
+    const [orders,setOrders] = useState([])
 
-//     useEffect(()=>{
-//         axios
-//         .get('http://jsonplaceholder.typicode.com/users')
-//         .then(res=>{
-//             setOrders(res.data);
-//         })
-//     })
+    useEffect(()=>{
+        axios
+        .get('http://jsonplaceholder.typicode.com/users')
+        .then(res=>{
+            setOrders(res.data);
+        })
+    })
     
-//     return(
-//         <div>
-//             {
-//                 orders.map(order=>(
-//                     <div className='flex-container'>
-//                         <div className='card'>
-//                             <img className='imgZ' src="https://picsum.photos/300"/>
-//                             <h1 className='row' key={order.id}>{order.name}</h1>
-//                             <button className='buttonZ'> Add this order</button>
-//                         </div>
-//                     </div>
-//                 ))
-//             }
-//         </div>
+    return(
+        <div>
+            {
+                orders.map(order=>(
+                    <div className='flex-container'>
+                        <div className='card'>
+                            <img className='imgZ' src="https://picsum.photos/300"/>
+                            <h1 className='row' key={order.id}>{order.name}</h1>
+                            <button className='buttonZ'> Add this order</button>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     
-//     )
-// }
+    )
+}
 
-// export default AllOrders;
+export default AllOrders;
